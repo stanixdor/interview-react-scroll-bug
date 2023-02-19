04d60c798386003688e0aacc825f9962dd6511f6
stanixdor <aritzenil@hotmail.com>
2023-02-19 15:53:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
 
-function App() {
+function App() 
+{
+  const scrollRef = React.useRef(null)
+
+  function Scroll()
+  {
+    scrollRef.current.scrollIntoView({behavior: 'smooth'})
+  }
+
+  React.useEffect(() =>
+  {
+    Scroll()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img src='https://images.pexels.com/photos/3791466/pexels-photo-3791466.jpeg' />
+      <img src='https://images.pexels.com/photos/1643409/pexels-photo-1643409.jpeg' />
+      <img src='https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg' />
+      <img src='https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg' />
+      <img src='https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg' />
+      <h1 ref={scrollRef}>SCROLLEAR AQUI</h1>
     </div>
   );
 }
